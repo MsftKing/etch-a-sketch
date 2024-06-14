11540c43ef84027b6615111bee0388d41679e16b
Ryan King <ryan.h.king@outlook.com>
2024-06-13 20:58:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// your JavaScript file
+const container = document.querySelector("#gridContainer");
+
+let numRows = 16;
+let numCols = 16; 
+
+let grid = document.createElement("div");
+
+for (X = 0; i < numRows; i++){
+    for (y = 0; i < mumCols; i++);
+    
+}
